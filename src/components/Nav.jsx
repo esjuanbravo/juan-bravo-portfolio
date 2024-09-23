ad1610c8f9eb2748871db7b4ee4e8ce0e48eb01a
esjuanbravo <esjuanbravo12@gmail.com>
2024-09-23 15:27:37 -0400

    header and title layout

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+function Nav() {
+  return (
+    <ul className="nav">
+      <li>
+        <a href="#">Mi trabajo</a>
+      </li>
+      <li>
+        <a href="#">Sobre mi</a>
+      </li>
+      <li>
+        <a href="#">Contactame</a>
+      </li>
+    </ul>
+  );
+}
+
+export default Nav;
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+function Title() {
+  return (
+    <div className="title__container">
+      <h1 className="title__span">
+        <span>Hola, Soy Juan.</span>
+        <span>Programador Full-Stack.</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Title;
